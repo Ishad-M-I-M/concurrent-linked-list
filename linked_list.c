75e5f152902f6312d686c108a0422a74123d2b6f
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2023-09-13 06:37:30 +0530

    Allocate more fair chance in choosing operation based on fraction allocated

--- linked_list.c
+++ linked_list.c
@@ -25,11 +25,11 @@ float m_member = 0.50;    // fraction of member operations from m
 float m_insert = 0.25;    // fraction of insert operations from m
 float m_delete = 0.25;    // fraction of delete operations from m
 
-int operation;
+float operation;
 double start_time, finish_time, time_elapsed;
 
 int main(void) {
-    double * time_arr = (double *)malloc(iterations * sizeof(double));
+    double *time_arr = (double *) malloc(iterations * sizeof(double));
     for (int j = 0; j < iterations; j++) {
         struct node *head = NULL;
         int i = 0;
@@ -46,17 +46,19 @@ int main(void) {
 
         start_time = clock();
         for (i = 0; i < m; i++) {
-            operation = rand() % 3;
+            operation = (rand() % m) / m; // random number between 0 and m
             int r = rand() % 65536;
-            if (operation == 0 && member_count > 0) {
+            if (operation < m_member && member_count > 0) {
                 member(r, head);
                 member_count--;
-            } else if (operation == 1 && insert_count > 0) {
+            } else if (m_member < operation < m_member + m_insert && insert_count > 0) {
                 insert(r, &head);
                 insert_count--;
-            } else if (operation == 2 && delete_count > 0){
+            } else if (m_member + m_insert < operation < m_member + m_insert + m_delete && delete_count > 0) {
                 delete(r, &head);
                 delete_count--;
+            } else {
+                i--;
             }
         }
         finish_time = clock();
